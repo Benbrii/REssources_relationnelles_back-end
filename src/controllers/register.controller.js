ab4ec7088c822dc0e7b9f405f12620e83b677b49
valentin pottier <valentin.vp.vp@gmail.com>
2021-02-15 16:52:16 +0100

    update register : verification email and password with regex and show a formlar error

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,9 +1,46 @@
 import {
-    insertNewRegister
+    insertNewRegister,GetVerifyEmail,hashPassword,VerifyPassword
 } from "../models/register.model";
 
 export const insertRegister = async (req, res) => {
-    let { lastName, firstName, email, motDePasse, birthDate } = req.body;
-    let register = await insertNewRegister({ lastName, firstName, email, motDePasse, birthDate });
-    res.json(register);
+   
+    let { lastName, firstName, email, password1,password2, birthDate } = req.body;
+    
+    // On verify dans la base de données que l'email n'existe pas déjà
+    const resultEmail = await GetVerifyEmail({ email });
+    
+    if (resultEmail[0].verifyEmail == 0){
+
+        //On verifie que le niveau de sécurité du mot de passe est suffisant et que le mots de passe de confirmation est correcte
+        const passwordVerify = await VerifyPassword({password1,password2})
+        if (passwordVerify == true){
+            
+            // On crypte le mots de passe
+            let passwordHashed = await hashPassword({password1});
+
+             //On insere les données dans la base de donnée
+            let register = await insertNewRegister({ lastName, firstName, email, passwordHashed, birthDate });
+            
+
+            //On verifie qu'une ligne a  bien était mise
+            if (register.affectedRows == 1){
+
+                res.json({ validation: true });
+
+            }else{
+
+                res.json({ validation: false });
+
+            }
+        }else{
+            res.json({ validation: false });
+        }
+        
+    }else{
+        res.json({ validation: false });
+    }
+ 
 }
+
+
+
--- src/models/register.model.js
+++ src/models/register.model.js
@@ -1,18 +1,96 @@
-import { insert } from "../utils/database";
+import { insert,query } from "../utils/database";
+import bcrypt from 'bcrypt'
 
-export const insertNewRegister = ({ lastName, firstName, email, motDePasse, birthDate }) => {
-    console.log(lastName, firstName, email, motDePasse, birthDate);
-
+export const insertNewRegister = ({ lastName, firstName, email, passwordHashed, birthDate }) => {
+    
     return new Promise((resolve, reject) => {
-        insert(
+        console.log("PASSWORD:",passwordHashed)
+     insert(
             `INSERT INTO compte (nom, prenom, email, motDePasse, date_de_naissance)
-             VALUES ( '${lastName}', '${firstName}', '${email}', '${motDePasse}', '${birthDate}')`,
+                VALUES ( '${lastName}', '${firstName}', '${email}', '${passwordHashed}', '${birthDate}')`,
             (error, insert) => {
                 if (error) reject(error);
-                resolve();
+                resolve(insert);
             }
         );
     });
 };
 
+export const GetVerifyEmail = ({email}) => {
+// On verifie que l'email n'est pas déja utilisé
+    return new Promise((resolve, reject) => {
+        const emailpattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        
+        if(email.match(emailpattern)){
+            
+            query(
+                `SELECT COUNT(email) as verifyEmail FROM compte WHERE email = '${email}'`,
+                (error, result) => {
+                    if (error) reject(error);
+                    resolve(result);
+                }
+            );
+        }else{
+            console.log("Veuillez entré un email valide")
+            reject(false)
+        }
+          
+    });
+};
 
+export const VerifyPassword = ({password1,password2}) => {
+//On verifie que le niveau de sécurité du mot de passe est suffisant et que le mots de passe de confirmation est correcte
+
+    return new Promise((resolve, reject) => {
+        //REGEX
+        const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
+       
+        if (password1.match(passwordPattern)){
+            
+            if (password1 == password2){
+                resolve(true);
+            }else{
+                console.log("Le mots de passe de confirmation est faux")
+                reject(false)
+            }
+        }else{
+            console.log("le mots de passe n'est pas valide: le mots de passe doit contenir entre 8 et 15 caracteres, une majuscule,une minuscule, un chiffre et un caractére speciale")
+            reject(false)
+        }
+    });
+};
+
+
+
+export const hashPassword = (password) => {
+//On Crypte le mot de passe
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password.password1, 10, function(err, hash) {
+            if (err) reject(err);
+            resolve(hash);
+        });
+
+    });
+};
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+/*Compare password 
+
+
+const comparePassword = async (password, hash) => {
+    try {
+        // Compare password
+        return await bcrypt.compare(password, hash);
+    } catch (error) {
+        console.log(error);
+    }
+
+    // Return false if error
+    return false;
+};
+*/ 
