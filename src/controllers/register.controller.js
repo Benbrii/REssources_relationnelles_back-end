7d5529e6e9c843c08b1d2e20f4947ea7e0eb3926
Benoît Briastre <benoit.briastre@gmail.com>
2021-02-23 14:53:39 +0100

    checking done ok

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,53 +1,53 @@
-import {insertNewRegister,GetVerifyEmail} from "../models/user.model";
+import { insertNewRegister, GetVerifyEmail } from "../models/user.model";
 import bcrypt from 'bcrypt'
 
-export const insertRegister = async (req, res) => { 
-    
-    let { lastName, firstName, email, password1,password2, birthDate, pseudo, adress, city, postalCode} = req.body;
-   
+export const insertRegister = async (req, res) => {
+
+    let { lastName, firstName, email, password1, password2, birthDate, pseudo, adress, city, postalCode } = req.body;
+
     // On verify dans la base de données que l'email n'existe pas déjà
-    if(lastName != null && firstName != null && email != null && password1 != null && password2 != null && birthDate != null && pseudo != null && adress != null && city != null  && postalCode != null){
-         
+    if (lastName != null && firstName != null && email != null && password1 != null && password2 != null && birthDate != null && pseudo != null && adress != null && city != null && postalCode != null) {
+
         const resultEmail = await GetVerifyEmail({ email });
-        if (resultEmail[0].verifyEmail == 0){
+        if (resultEmail[0].verifyemail === '0') {
 
             //On verifie que le niveau de sécurité du mot de passe est suffisant et que le mots de passe de confirmation est correcte
-            const passwordVerify = await VerifyPassword({password1,password2})
-            if (passwordVerify == true){
-                
+            const passwordVerify = await VerifyPassword({ password1, password2 })
+            if (passwordVerify == true) {
+
                 // On crypte le mots de passe
-                let passwordHashed = await hashPassword({password1});
+                let passwordHashed = await hashPassword({ password1 });
 
                 //On insere les données dans la base de donnée
-                let register = await insertNewRegister({ lastName, firstName, email, passwordHashed, birthDate,pseudo,adress,city,postalCode});
-                
+                let register = await insertNewRegister({ lastName, firstName, email, passwordHashed, birthDate, pseudo, adress, city, postalCode });
+
                 //On verifie qu'une ligne a  bien était mise
-                if (register.affectedRows == 1){
+                if (register.rowCount === 1) {
 
                     res.json({ validation: true });
-                    
-                }else{
-                    res.json({ validation: false,message:"OUPS...Une Erreur est survenu l'utilisateur n'a pas était enregistré" });
-                    console.log("ERROR",1)
+
+                } else {
+                    res.json({ validation: false, message: "OUPS...Une Erreur est survenu l'utilisateur n'a pas était enregistré" });
+                    console.log("ERROR", 1)
                 }
-            }else{
-                res.json({ validation: false,message:passwordVerify});
-                console.log("ERROR",2)
+            } else {
+                res.json({ validation: false, message: passwordVerify });
+                console.log("ERROR", 2)
             }
-        }else{
+        } else {
             res.json({ validation: false });
-            console.log("ERROR",3)
+            console.log("ERROR", 3)
         }
-    }else{  
+    } else {
         res.json({ validation: false });
-        console.log("ERROR",4)
+        console.log("ERROR", 4)
     }
 }
 
 //On Crypte le mot de passe
 const hashPassword = (password) => {
     return new Promise((resolve, reject) => {
-        bcrypt.hash(password.password1, 10, function(err, hash) {
+        bcrypt.hash(password.password1, 10, function (err, hash) {
             if (err) reject(err);
             resolve(hash);
         });
@@ -55,18 +55,18 @@ const hashPassword = (password) => {
 };
 
 //On verifie que le niveau de sécurité du mot de passe est suffisant et que le mots de passe de confirmation est correcte
-const VerifyPassword = ({password1,password2}) => {
+const VerifyPassword = ({ password1, password2 }) => {
     return new Promise((resolve, reject) => {
         //REGEX
         const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
-        if (password1.match(passwordPattern)){
-            if (password1 == password2){
+        if (password1.match(passwordPattern)) {
+            if (password1 == password2) {
                 resolve(true);
-            }else{
+            } else {
                 console.log("Le mots de passe de confirmation est faux")
                 reject("Le mots de passe de confirmation est faux")
             }
-        }else{
+        } else {
             console.log("le mots de passe n'est pas valide: le mots de passe doit contenir entre 8 et 15 caracteres, une majuscule,une minuscule, un chiffre et un caractére speciale")
             reject("le mots de passe n'est pas valide: le mots de passe doit contenir entre 8 et 15 caracteres, une majuscule,une minuscule, un chiffre et un caractére speciale")
         }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,51 +1,51 @@
-import { insert,query } from "../utils/database";
+import { insert, query } from "../utils/database";
 
 
 
-export const GetVerifyEmail = ({email}) => {
-// On verifie que l'email n'est pas déja utilisé
+export const GetVerifyEmail = ({ email }) => {
+    // On verifie que l'email n'est pas déja utilisé
     return new Promise((resolve, reject) => {
         const emailpattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-        
-        if(email.match(emailpattern)){
-            
+
+        if (email.match(emailpattern)) {
+
             query(
                 `SELECT COUNT(email) as verifyEmail FROM compte WHERE email = '${email}'`,
                 (error, result) => {
                     if (error) reject("L'email est déja utilisé");
-                    resolve(result);
+                    resolve(result.rows && result.rows.length === 0 ? [] : result.rows);
                 });
-        }else{
-            console.log("Veuillez entré un email valide")
-            reject("Veuillez entré un email valide")
+        } else {
+            console.log("Veuillez entrer un email valide")
+            reject("Veuillez entrer un email valide")
         }
     });
 };
 
-export const GetAccount = ({email}) => {
-// On verifie que l'email n'est pas déja utilisé
+export const GetAccount = ({ email }) => {
+    // On verifie que l'email n'est pas déja utilisé
     return new Promise((resolve, reject) => {
-   
-            query(
-                `SELECT * FROM compte WHERE email = '${email}'`,
-                (error, result) => {
-                    if (error) reject(error);
-                    resolve(result);
-                }
-            );
+
+        query(
+            `SELECT * FROM compte WHERE email = '${email}'`,
+            (error, result) => {
+                if (error) reject(error);
+                resolve(result.rows && result.rows.length === 0 ? [] : result.rows);
+            }
+        );
     });
 };
 
-   
-export const insertNewRegister = ({ lastName, firstName, email, passwordHashed, birthDate,pseudo,adress,city,postalCode}) => {
- // On insert les données de l'utilisateur dans la base de données
-    return new Promise((resolve, reject) => {  
-     insert(
-            `INSERT INTO compte (nom, prenom, email, motDePasse, date_de_naissance,pseudo,adresse,ville,code_postal,authlevel)
+
+export const insertNewRegister = ({ lastName, firstName, email, passwordHashed, birthDate, pseudo, adress, city, postalCode }) => {
+    // On insert les données de l'utilisateur dans la base de données
+    return new Promise((resolve, reject) => {
+        query(
+            `INSERT INTO compte (nom, prenom, email, motdepasse, date_de_naissance, pseudo, adresse, ville, code_postal, authlevel)
             VALUES ( '${lastName}', '${firstName}', '${email}', '${passwordHashed}', '${birthDate}', '${pseudo}', '${adress}', '${city}', ${postalCode}, 1)`,
-            (error, insert) => {
+            (error, result) => {
                 if (error) reject(error);
-                resolve(insert);
+                resolve(result);
             }
         );
     });
